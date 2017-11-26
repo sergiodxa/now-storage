0bf0a3696c80e83dd7d04562d9250aa882699c09
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-11-26 13:52:15 -0300

    Add tests and Travis

---
+++ index.test.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch');
+const { upload } = require('.');
+
+describe('File upload', () => {
+  it('should upload a file and get a new URL', async () => {
+    const content = 'This is a file uploaded with now-storage.';
+
+    const { url } = await upload(
+      process.env.NOW_TOKEN,
+      {
+        name: 'my-file.txt',
+        content
+      },
+      { deploymentName: 'now-storage-test' }
+    );
+
+    const response = await fetch(`https://${url}`);
+    const body = await response.text();
+
+    expect(content).toBe(body);
+  });
+
+  it('should upload a file to a team and get a new URL', async () => {
+    const content = JSON.stringify({
+      key: 'value',
+      key2: 123
+    });
+
+    const { url } = await upload(
+      process.env.NOW_TOKEN,
+      {
+        name: 'my-file.json',
+        content
+      },
+      { deploymentName: 'now-storage-test', teamId: 'now-storage' }
+    );
+
+    const response = await fetch(`https://${url}`);
+    const body = await response.text();
+
+    expect(content).toBe(body);
+  });
+});
